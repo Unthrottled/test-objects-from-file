0b7cbd07269b04769bdd22ab6a730345708ed22f
cyclic-reference <alex@cyclicreference.space>
2017-05-07 11:07:36 -0500

    Initial Commit

---
+++ src/main/java/io/acari/TestObjectsFromFileApplication.java
@@ -0,0 +1,12 @@
+package io.acari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestObjectsFromFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestObjectsFromFileApplication.class, args);
+	}
+}
